4bca39bc14522f466c163d13b64ac3c3c4017d10
Atle Magnussen <atlemagnussen@gmail.com>
2024-06-23 09:44:22 +0200

    use hosted service

--- server/consoleApp/Program.cs
+++ server/consoleApp/Program.cs
@@ -1,37 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using System.Security.Claims;
-using System.Security.Cryptography;
 using System.Text.Json;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Tokens;
+using Microsoft.Extensions.Hosting;
 using net8auth.consoleApp;
 using net8auth.model;
 using net8auth.model.Test;
-using net8auth.model.Tokens;
 
 Console.WriteLine("Hello, World!");
+var builder = Host.CreateApplicationBuilder(args);
 
-var builder = new ConfigurationBuilder();
-IConfiguration configuration = builder.GetConfigCmd(args);
+builder.Services.AddAuthenticationServices();
+builder.Services.AddOptionsConfiguration(builder.Configuration);
 
-var services = new ServiceCollection();
-services.AddAuthenticationServices();
-services.AddOptionsConfiguration(configuration);
-services.AddSingleton(configuration);
+var host = builder.Build();
 
-var sp = services.BuildServiceProvider();
+var getCryptoKeys = host.Services.GetRequiredService<GetCryptoKeys>();
 
-// var getCryptoKeys = sp.GetService<GetCryptoKeys>()!;
-// CryptoKeys keys = getCryptoKeys.FromConfig();
-// Console.WriteLine(keys.ToString());
+CryptoKeys keys = getCryptoKeys.FromConfig();
+Console.WriteLine(keys.ToString());
+
+//await host.RunAsync();
 
 // var tokenService = sp.GetService<ITokenService>()!;
 // var jwt = await tokenService.CreateAndSignJwt(new ClaimsPrincipal());
 // Console.WriteLine("jwt");
 // Console.WriteLine(jwt);
 
-var key = CreateKeys.CreateEcKey();
-var keyStr = JsonSerializer.Serialize(key);
-Console.WriteLine(keyStr);
+// var key = CreateKeys.CreateEcKey();
+// var keyStr = JsonSerializer.Serialize(key);
+// Console.WriteLine(keyStr);
